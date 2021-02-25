b522687b31b00c796d15785eb441b9119c062ffb
nallelyfm88 <nallely.flores@enroutesystems.com>
2021-02-25 13:30:56 -0600

    fix: fix description

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/nallelyfm88/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
