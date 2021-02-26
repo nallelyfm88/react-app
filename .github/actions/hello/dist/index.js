b3352e2215c6057fce9c6b4f46ff75b8464b1ad9
nallelyfm88 <nallely.flores@enroutesystems.com>
2021-02-26 10:39:17 -0600

    fix: create new wf

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -604,15 +604,22 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw new Error("some error message");
-  const name = core.getInput("who-to-greet");
+  // throw new Error("some error message");
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
 
+  const name = core.getInput("who-to-greet");
+  core.setSecret("name");
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time ", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(eeror.message);
 }
